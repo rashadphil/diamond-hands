379d24a553eb9c67f0ab56b20f3f6e6aa79e2ecb
rashad <rashadphilizaire@gmail.com>
2020-12-30 00:12:32 -0500

    form now uses react hooks

--- diamondhands/src/NewOption/OptionForm/index.tsx
+++ diamondhands/src/NewOption/OptionForm/index.tsx
@@ -1,54 +1,44 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import FormButton from "../FormButton";
 import Modal from "./Modal";
-import CardWheel from "../../OptionCard/CardWheel";
 
-class OptionForm extends React.Component {
-    state = { isShown: false, newCard: {} };
-    showModal = () => {
-        this.setState({ isShown: true });
-        this.toggleScrollLock();
-    };
-    toggleScrollLock = () => {
-        document.querySelector("html")?.classList.toggle("scroll-lock");
-    };
-    closeModal = () => {
-        this.setState({ isShown: false });
-        this.toggleScrollLock();
-    };
-    onSubmit = (event) => {
-        event.preventDefault(event);
-        this.setState({
-            isShown: false,
-            newCard: {
-                strike: event.target.strike.value,
-                ticker: event.target.ticker.value,
-                expiration: event.target.expiration.value,
-            },
-        });
-        // console.log(event.target.strike.value);
-        // console.log(event.target.ticker.value);
-        // console.log(event.target.expiration.value);
-        // console.log(this.state);
-        console.log(event.target.value);
-    };
-    render() {
-        return (
-            <React.Fragment>
-                <FormButton
-                    showModal={this.showModal}
-                    closeModal={this.closeModal}
-                ></FormButton>
-                {this.state.isShown ? (
-                    <Modal
-                        closeModal={this.closeModal}
-                        onSubmit={this.onSubmit}
-                    ></Modal>
-                ) : null}
-            </React.Fragment>
-        );
+function OptionForm() {
+    const [isShown, formVisible] = useState(false);
+    function showModal() {
+        formVisible(true);
+        toggleScrollLock();
     }
+    function toggleScrollLock() {
+        document.querySelector("html")?.classList.toggle("scroll-lock");
+    }
+    function closeModal() {
+        formVisible(false);
+        toggleScrollLock();
+    }
+    function onSubmit(event) {
+        event.preventDefault(event);
+        formVisible(false);
+        // this.setState({
+        //     isShown: false,
+        //     newCard: {
+        //         strike: event.target.strike.value,
+        //         ticker: event.target.ticker.value,
+        //         expiration: event.target.expiration.value,
+        //     },
+        // });
+    }
+    return (
+        <React.Fragment>
+            <FormButton
+                showModal={showModal}
+                closeModal={closeModal}
+            ></FormButton>
+            {isShown ? (
+                <Modal closeModal={closeModal} onSubmit={onSubmit}></Modal>
+            ) : null}
+        </React.Fragment>
+    );
 }
 
 export default OptionForm;
